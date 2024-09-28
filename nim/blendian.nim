b325aa3ab322193745c338be6b5ba20ecde2dab0
Jacques Heunis <git@jacquesheunis.com>
2024-09-28 22:38:47 +0100

    Add some output filtering flags to BLendian

--- nim/blendian.nim
+++ nim/blendian.nim
@@ -40,7 +40,7 @@ proc printSequence[T: SomeInteger](vals: seq[T]) =
     stdout.write("\n")
 
 
-proc printValue[T: SomeInteger](input: string) =
+proc printValue[T: SomeInteger](input: string, printWordHeader: bool, allowLE: bool, allowBE: bool) =
     assert len(input) mod 2 == 0
     if len(input) mod sizeof(T)*2 != 0:
         stderr.write("TODO: No 2-byte output!")
@@ -55,12 +55,21 @@ proc printValue[T: SomeInteger](input: string) =
         vals_be.add(val_be)
         vals_le.add(val_le)
 
-    stdout.writeLine(fmt"{sizeof(T)*8}-bit words:")
-    stdout.write("   Big endian: ")
-    printSequence(vals_be)
+    assert allowLE or allowBE
+    let printEndianHeader = allowLE and allowBE
 
-    stdout.write("Little endian: ")
-    printSequence(vals_le)
+    if printWordHeader:
+        stdout.writeLine(fmt"{sizeof(T)*8}-bit words:")
+
+    if allowBE:
+        if printEndianHeader:
+            stdout.write("   Big endian: ")
+        printSequence(vals_be)
+
+    if allowLE:
+        if printEndianHeader:
+            stdout.write("Little endian: ")
+        printSequence(vals_le)
 
     stdout.write("\n")
 
@@ -68,20 +77,47 @@ proc printValue[T: SomeInteger](input: string) =
 proc main() =
     let args = os.commandLineParams()
     if (len(args) == 0) or (args[0] == "--help"):
-        stderr.writeLine("blendian: Display hex strings as a sequence of integers in both big- and little endian")
+        stderr.writeLine("BLendian: Display hex strings as a sequence of integers in both big- and little endian")
         stderr.writeLine("")
-        stderr.writeLine("Usage: blendian <hex-string>")
-        return
+        stderr.writeLine("Usage: blendian [-16|-32|-64] [-be|-le] <hex-string>")
+        quit(1)
+
+    let only16 = ("-16" in args)
+    let only32 = ("-32" in args)
+    let only64 = ("-64" in args)
+    let onlyLE = ("-le" in args)
+    let onlyBE = ("-be" in args)
+
+    if (only16 and only32) or (only16 and only64) or (only32 and only64):
+        stderr.writeLine("Cannot specify 2 different word sizes to display, only one of -16, -32, or -64 is allowed")
+        quit(1)
+
+    let sizeSpecified = (only16 or only32 or only64)
+    let allow16 = only16 or not sizeSpecified
+    let allow32 = only32 or not sizeSpecified
+    let allow64 = only64 or not sizeSpecified
+
+    let allowLE = onlyLE or (not onlyLE and not onlyBE)
+    let allowBE = onlyBE or (not onlyLE and not onlyBE)
+    assert allowLE or allowBE
 
     # TODO: Also, take this from stdin so that we can pipe things together?
-    # TODO: Allow the user to specify an endianness, base and int size, and then only output those things, so this can be piped to other programs?
-    var input = args[0]
-    input.removePrefix("0x")
-    if len(input) mod 2 == 1:
-        input = "0" & input
+    for input in args:
+        if input.startsWith('-'):
+            continue
 
-    printValue[uint64](input)
-    printValue[uint32](input)
-    printValue[uint16](input)
+        var hex = input
+        hex.removePrefix("0x")
+        if len(hex) mod 2 == 1:
+            hex = "0" & hex
+
+        if allow16:
+            printValue[uint64](hex, not sizeSpecified, allowLE, allowBE)
+
+        if allow32:
+            printValue[uint32](hex, not sizeSpecified, allowLE, allowBE)
+
+        if allow64:
+            printValue[uint16](hex, not sizeSpecified, allowLE, allowBE)
 
 main()
