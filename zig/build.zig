122e5f38e6f40a7c1a992064e5e47500c316a579
Jacques Heunis <git@jacquesheunis.com>
2024-11-26 18:56:08 +0000

    Remove zig dec2hex & hex2dec implementations

--- zig/build.zig
+++ zig/build.zig
@@ -11,8 +11,5 @@ fn createExeStep(b: *zigbuild.Builder, name: []const u8, rootFilePath: []const u
 pub fn build(b: *zigbuild.Builder) void {
     const all_step = b.step("all", "Build all executables");
 
-    all_step.dependOn(&createExeStep(b, "hex2dec", "src/hex2dec.zig").step);
-    all_step.dependOn(&createExeStep(b, "dec2hex", "src/dec2hex.zig").step);
-
     b.default_step.dependOn(all_step);
 }
