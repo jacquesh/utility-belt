2be01a04ffadde9c1ca8efa755f0373bfb688f44
Jacques Heunis <git@jacquesheunis.com>
2026-06-13 10:37:06 +0100

    Add automatic millisecond detection to unix2utc

--- rust/src/bin/unix2utc.rs
+++ rust/src/bin/unix2utc.rs
@@ -1,18 +1,24 @@
-use std::{env, io, process};
 use atty::Stream;
-use chrono::Utc;
 use chrono::offset::TimeZone;
+use chrono::{Datelike, Utc};
 use getopts::Options;
+use std::{env, io, process};
 
 fn print_usage(program: &str, opts: Options) {
     let brief = format!("Usage: {} [OPTIONS] [TIMESTAMP]...", program);
     print!("{}", opts.usage(&brief));
 
-    println!("");
+    println!();
     println!("If stdin has been redirected then each line of stdin will be separately decoded and printed");
-    println!("");
-    println!("TIMESTAMP should be an integer representing seconds or milliseconds since the Unix epoch.");
-    println!("TIMESTAMP can also be the string 'now' (case-insensitive) to output the curent time");
+    println!();
+    println!(
+        "TIMESTAMP should be an integer representing seconds or milliseconds since the Unix epoch."
+    );
+    println!(
+        "TIMESTAMP can also be the string 'now' (case-insensitive) to output the curent time."
+    );
+    println!();
+    println!("If neither --millis nor --seconds is passed, seconds is assumed unless this results in a timestamp after the year 10,000.");
 }
 
 fn main() {
@@ -20,12 +26,21 @@ fn main() {
     let program = &args[0];
     let mut opts_spec = Options::new();
     opts_spec.optflag("h", "help", "print this help menu");
-    opts_spec.optflag("m", "millis", "use the input as the number of milliseconds since the unix epoch, rather than the number of seconds");
-    opts_spec.optflag("s", "iso", "use the standard ISO-8601/RFC-3339 format when formatting the output text");
+    opts_spec.optflag(
+        "m",
+        "millis",
+        "interpret input as a number of milliseconds since the unix epoch",
+    );
+    opts_spec.optflag(
+        "s",
+        "seconds",
+        "interpret input as a number of seconds since the unix epoch",
+    );
+    opts_spec.optflag("i", "iso", "output in standard ISO-8601/RFC-3339 format");
     let opts = match opts_spec.parse(&args[1..]) {
         Ok(o) => o,
         Err(e) => {
-            eprintln!("{}", e.to_string());
+            eprintln!("{}", e);
             eprintln!("Try '{} --help' for more information", program);
             process::exit(1);
         }
@@ -33,7 +48,7 @@ fn main() {
 
     if opts.opt_present("h") {
         println!("Convert a unix timestamp into an equivalent human-readable date-time string");
-        println!("");
+        println!();
 
         print_usage(program, opts_spec);
         return;
@@ -45,7 +60,7 @@ fn main() {
 
     if is_stdin_tty && opts.free.is_empty() {
         eprintln!("No input values provided");
-        eprintln!("");
+        eprintln!();
         print_usage(program, opts_spec);
         process::exit(1);
     }
@@ -64,12 +79,12 @@ fn main() {
     let mut input = String::new();
     loop {
         if let Err(e) = io::stdin().read_line(&mut input) {
-            eprintln!("Error: {}", e.to_string());
+            eprintln!("Error: {}", e);
             break;
         }
 
         let trimmed = input.trim();
-        if trimmed.len() == 0 {
+        if trimmed.is_empty() {
             break;
         }
 
@@ -84,11 +99,17 @@ fn process_input(input: &str, use_millis: bool, use_iso_format: bool) {
         Err(e) => {
             if input.eq_ignore_ascii_case("now") {
                 match use_millis {
-                    true => (Utc::now().timestamp() * 1000) + (Utc::now().timestamp_subsec_millis() as i64),
+                    true => {
+                        (Utc::now().timestamp() * 1000)
+                            + (Utc::now().timestamp_subsec_millis() as i64)
+                    }
                     false => Utc::now().timestamp(),
                 }
             } else {
-                eprintln!("Bad input: \"{}\" is not a valid unix timestamp. Error: '{}'", input, e);
+                eprintln!(
+                    "Bad input: \"{}\" is not a valid unix timestamp. Error: '{}'",
+                    input, e
+                );
                 return;
             }
         }
@@ -96,17 +117,22 @@ fn process_input(input: &str, use_millis: bool, use_iso_format: bool) {
 
     let output = match use_millis {
         true => {
-            let seconds = input_timestamp/1000;
+            let seconds = input_timestamp / 1000;
             let nanos = ((input_timestamp % 1000) * 1_000_000) as u32;
             Utc.timestamp(seconds, nanos)
-        },
-        false => Utc.timestamp(input_timestamp, 0)
+        }
+        false => {
+            let candidate = Utc.timestamp(input_timestamp, 0);
+            if candidate.year() > 10000 {
+                return process_input(input, true, use_iso_format);
+            }
+            candidate
+        }
     };
 
     if use_iso_format {
         println!("{}", output.to_rfc3339());
     } else {
-        println!("{}", output.to_string());
+        println!("{}", output);
     }
 }
-
